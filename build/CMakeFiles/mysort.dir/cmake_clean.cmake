05d8265bfb7f82717ff4a18ea34073ff22f06533
changwei.cheng <1441159427@qq.com>
2024-06-16 20:37:54 +0800

    1 add makefile

---
+++ build/CMakeFiles/mysort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysort.dir/mysort.cpp.o"
+  "mysort"
+  "mysort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mysort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
